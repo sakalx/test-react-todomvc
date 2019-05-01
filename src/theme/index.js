3b79fc589d7d46889734c60f0261388b11b9371f
sakal <sakals@mail.ua>
2019-04-30 20:33:53 -0400

    init: Theme and Utils.
    Signed-off-by: sakal <sakals@mail.ua>

---
+++ src/theme/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export default styled('div')`
+  --background-color: #fff;
+  --disabled-color: #bebebe;
+  --primary-color: #2196f3;
+  --primary-dark-color: #1769aa;
+  --primary-light-color: #4dabf5;
+  --secondary-color: #f50057;
+  --secondary-dark-color: #ab003c;
+  --secondary-light-color: #f73378;
+`;
